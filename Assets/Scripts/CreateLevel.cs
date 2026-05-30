9fcd2e9178470e05da67a19a5866141623a66f78
Federico Ignacio Ruckauf <100965939+Fedir03@users.noreply.github.com>
2026-05-30 12:43:49 +0200

    code: Incorporate map borders and walls

--- Assets/Scripts/CreateLevel.cs
+++ Assets/Scripts/CreateLevel.cs
@@ -2,35 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-using System;
-using UnityEngine.Tilemaps;
 
 public class CreateLevel : MonoBehaviour
 {
-    public GameObject player;                   // Reference to the player object.
-                                                // We need to position it according to the level.
-    public GameObject ground, wall, box, goal;  // References to objects we need to instantiate to
-
+    public GameObject player;
+    public GameObject ground, wall, box, goal;
     public float tileSize = 2.0f;
-    
-    // Start is called before the first frame update
+
+    public GameObject[] wallPrefabs;
+    public GameObject doorPrefab;
+    public GameObject invisibleBorder;
+
     void Start()
     {
-        // dataPath is the directory path to the Assets in the project
-        // We want to load file map.txt inside directory Maps.
         string filename = Application.dataPath + "/Maps/map.txt";
 
         if (File.Exists(filename))
         {
-            // Read map file line by line tokenizing them into the numbers we need.
-            // Specifically, width, height and the tile ids for all positions in the map.
-
             TextReader reader = File.OpenText(filename);
             string line = reader.ReadLine();
             string[] tokens = line.Split(' ');
-            int width, height;
-            width = int.Parse(tokens[0]);
-            height = int.Parse(tokens[1]);
+            int width = int.Parse(tokens[0]);
+            int height = int.Parse(tokens[1]);
+
             for (int y = 0; y < height; y++)
             {
                 line = reader.ReadLine();
@@ -39,12 +33,9 @@ public class CreateLevel : MonoBehaviour
                 {
                     int tile = int.Parse(tokens[x]);
 
-                    // All tiles will have a ground instance under them. We instantiate it here.
                     GameObject obj = Instantiate(ground, new Vector3(x * tileSize, 0.0f, y * tileSize), transform.rotation);
-                    // All instances created by this script end as children of the object that contains the script.
                     obj.transform.parent = transform;
 
-                    // Now, for objects other than the player we spawn an instance.
                     switch (tile)
                     {
                         case 2:
@@ -65,10 +56,64 @@ public class CreateLevel : MonoBehaviour
                     }
                 }
             }
+
+            int doorX = width / 2;
+
+            for (int x = -1; x <= width; x++)
+            {
+                for (int y = -1; y <= height; y++)
+                {
+                    if ((x == -1 && y == -1) || (x == -1 && y == height) || (x == width && y == -1) || (x == width && y == height))
+                    {
+                        continue;
+                    }
+
+                    if (x == -1 || x == width || y == -1 || y == height)
+                    {
+                        GameObject borderPrefab = null;
+                        Quaternion spawnRotation = transform.rotation;
+
+                        if (x == width)
+                        {
+                            if (wallPrefabs != null && wallPrefabs.Length > 0)
+                            {
+                                borderPrefab = wallPrefabs[Random.Range(0, wallPrefabs.Length)];
+                            }
+                            spawnRotation *= Quaternion.Euler(0, 90, 0);
+                        }
+                        else if (x == -1)
+                        {
+                            borderPrefab = invisibleBorder;
+                            spawnRotation *= Quaternion.Euler(0, 90, 0);
+                        }
+                        else if (y == height)
+                        {
+                            if (x == doorX && doorPrefab != null)
+                            {
+                                borderPrefab = doorPrefab;
+                            }
+                            else if (wallPrefabs != null && wallPrefabs.Length > 0)
+                            {
+                                borderPrefab = wallPrefabs[Random.Range(0, wallPrefabs.Length)];
+                            }
+                        }
+                        else if (y == -1)
+                        {
+                            borderPrefab = invisibleBorder;
+                        }
+
+                        if (borderPrefab != null)
+                        {
+                            GameObject borderObj = Instantiate(borderPrefab, new Vector3(x * tileSize, 0.0f, y * tileSize), spawnRotation);
+                            borderObj.transform.parent = transform;
+                            borderObj.tag = "Wall";
+                        }
+                    }
+                }
+            }
         }
         else
         {
-            // Hopefully this should not happen. But just in case ...
             Debug.Log("Map file could not be found!!!");
         }
     }
